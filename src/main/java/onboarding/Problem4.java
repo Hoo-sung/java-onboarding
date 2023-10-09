ef62b627fde294c01b21e6fed5a2c576d29126b0
Hoo-sung <gntjd135@gmail.com>
2023-10-09 15:54:47 +0900

    문제 4: revert 함수 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for(int i=0;i<word.length();i++){
+            char c = word.charAt(i);
+            if(c>='a'&& c<='z')
+                answer+=revert(c,'a');
+            else if(c>='A'&& c <='Z')
+                answer+=revert(c,'A');
+            else
+                answer+=c;
+        }
+        return answer;
+    }
+
+    public static char revert(char change, char k){
+        char answer=(char) ((k+k+25)-change);
         return answer;
     }
 }

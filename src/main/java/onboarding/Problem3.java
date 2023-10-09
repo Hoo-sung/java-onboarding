7c21b03b8c11395c2cadb711f37faa80d7fd8d0e
Hoo-sung <gntjd135@gmail.com>
2023-10-09 15:29:06 +0900

    문제 3번: clapTimes 함수 작성

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        
+        for(int i=1;i<=number;i++){
+            answer+=clapTimes(i);
+        }
         return answer;
+    }
+
+    public static int clapTimes(int num){
+        int clapNum=0;
+        while(num>=1){
+            if(num%10 ==3 || num%10 == 6 || num%10 ==9){
+                clapNum++;
+            }
+            num=num/10;
+        }
+        return clapNum;
     }
 }

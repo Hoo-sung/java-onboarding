c32d1e08eae9aa8cfc86ef834a62bd2b5bfabcbc
Hoo-sung <gntjd135@gmail.com>
2023-10-11 05:11:33 +0900

    Feat: find_duplicated_email_address 함수 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,44 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = List.of();
+
+        answer = find_duplicated_email_address(forms);
+        Collections.sort(answer);
         return answer;
     }
+
+    public static List<String> find_duplicated_email_address(List<List<String>> forms) {
+
+        Map<String, HashSet<String>> matched_address_hash = new HashMap<>();
+        Set<String> duplicated_email_address = new HashSet<>();
+
+        for (List<String> form : forms) {
+
+            String nickName = form.get(1);
+            String email = form.get(0);
+
+            for (int i = 0; i < nickName.length() - 1; i++) {
+
+                String sub_str = nickName.substring(i, i + 2);
+
+                HashSet<String> matched_email_address = matched_address_hash.getOrDefault(sub_str, new HashSet<>());
+                matched_email_address.add(email);
+
+                if (matched_email_address.size() == 2) {
+                    duplicated_email_address.addAll(matched_email_address);
+                } else if (matched_email_address.size() > 2) {
+                    duplicated_email_address.add(email);
+                }
+
+                matched_address_hash.put(sub_str, matched_email_address);
+            }
+        }
+
+        return new ArrayList<>(duplicated_email_address);
+    }
+
 }
